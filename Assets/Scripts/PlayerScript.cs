33e264a339ce4ac6e435f7150ead43720c780ac0
adachi <adachi@dorasu.com>
2016-08-24 20:21:22 +0900

    Finished Dorasu Gamejam6

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int			_minite;
+	public float		_second;
+
+	public float _x;
+	public float _y;
+
+	Vector3 _pos;
+
+	public Camera		_camera;
+
+	private HingeJoint	_joint;
+	private Rigidbody	_rb;
+	private GameObject	_gameobject;
+
+	private bool		_fInit;
+
+	public float _time;
+	public float _time2;
+
+	// Use this for initialization
+	void Start () {
+
+		_second = 0;
+		_joint = GetComponent<HingeJoint> ();
+		_time = Random.Range(3, 10);
+		_time2 = _time + 3;
+	}
+
+	// Update is called once per frame
+	void Update () {
+//		TouchInfo info = AppUtil.GetTouch();
+//		if (info == TouchInfo.Began)
+//		{
+//			if( _fInit == false ) {
+//				_fInit = true;
+//			}
+//		}
+
+		if( Time.timeScale < 0 ) { return; }
+
+		_second += Time.deltaTime;
+		Debug.Log(">>_time = "+_time+", _time2="+_time2+", _second="+_second);
+		if( _time <=_second ) {
+			_minite++;
+//			_second -= 60.0f;
+			_joint.breakForce = 1;
+//			Application.LoadLevel ("Title");
+		}
+
+//		if (_time2 <= _second) {
+//			Application.LoadLevel ("Title");
+//		}
+//		_timeText.text = "TITLE SCENE : " + _minite.ToString ("00") + ":" + _second.ToString ("00");
+	}
+}
---
+++ Assets/Scripts/SceneText.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class SceneText : MonoBehaviour {
+
+	private int		_minite;
+	private float	_second;
+//	public Text		_timeText;
+
+	private bool _fInit;
+
+	// Use this for initialization
+	void Start () {
+		_minite = 0;
+		_second = 0;
+//		_timeText = GetComponent<Text>();
+		_fInit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TouchInfo info = AppUtil.GetTouch();
+		if (info == TouchInfo.Began)
+		{
+			if( _fInit == false ) {
+				Application.LoadLevel ("Main");
+				_fInit = true;
+			}
+		}
+
+//		if( !_fInit ) { return; }
+//		if( Time.timeScale < 0 ) { return; }
+//
+//		_second += Time.deltaTime;
+//		if( 5.0f <=_second ) {
+//			_minite++;
+//			_second -= 60.0f;
+//			Application.LoadLevel ("Main");
+//		}
+//		_timeText.text = "TITLE SCENE : " + _minite.ToString ("00") + ":" + _second.ToString ("00");
+	}
+}
